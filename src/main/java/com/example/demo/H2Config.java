8c0f7e3d4f27bf3400edd9c5ddd2f85d976b5f21
kahar <ryszard.wojtkowiak92@gmail.com>
2019-10-18 11:30:19 +0200

    initial commit

---
+++ src/main/java/com/example/demo/H2Config.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class H2Config {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/liquibase/liquibase-changeLog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+}
